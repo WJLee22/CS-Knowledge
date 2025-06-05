df4d9b04bc2264da5422b3ab2feaafe7726b7bbb
wjlee <000jun2626@gmail.com>
2025-06-05 20:09:15 +0900

    SpringBoot:  Three ways to Configure bean(Java based Configuration)

--- src/main/java/kr/ac/hansung/cse/hellospringboot/HelloSpringBootApplication.java
+++ src/main/java/kr/ac/hansung/cse/hellospringboot/HelloSpringBootApplication.java
@@ -1,7 +1,9 @@
 package kr.ac.hansung.cse.hellospringboot;
 
+import kr.ac.hansung.cse.hellospringboot.service.MyService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 // 스프링부트 앱에서는 반드시 필요한 어노테이션(이 안에 @EnableAutoConfiguration, @ComponentScan 등 주요 애노테이션들이 포함되어 있다.)
 @SpringBootApplication
@@ -11,7 +13,11 @@ public class HelloSpringBootApplication {
     // main메서드는 스프링부트 애플리케이션을 실행하는 진입점이다.
     // 이때 jar 파일로 패키징되어(톰켓서버랑 같이) jar파일이 만들어지고 이 jar파일이 실행되면 스프링부트 애플리케이션이 시작된다.
     public static void main(String[] args) {
-        SpringApplication.run(HelloSpringBootApplication.class, args);
-    }
+        ApplicationContext context = SpringApplication.run(HelloSpringBootApplication.class, args);
 
+        // 메서드의 이름이 빈의 id.
+        MyService myService = (MyService) context.getBean("myService");
+        System.out.println(myService.getMessage());
+
+    }
 }
---
+++ src/main/java/kr/ac/hansung/cse/hellospringboot/service/MyService.java
@@ -0,0 +1,8 @@
+package kr.ac.hansung.cse.hellospringboot.service;
+
+public class MyService {
+
+    public String getMessage() {
+        return "Hello from MyService";
+    }
+}
